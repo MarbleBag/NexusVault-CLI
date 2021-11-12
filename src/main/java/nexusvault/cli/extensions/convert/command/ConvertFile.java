8276ba344c08b7a374371cefa993438c0d00839a
MarbleBag <12249693+MarbleBag@users.noreply.github.com>
2021-11-12 14:29:09 +0100

    Add cmd for file conversion

---
+++ src/main/java/nexusvault/cli/extensions/convert/command/ConvertFile.java
@@ -0,0 +1,115 @@
+package nexusvault.cli.extensions.convert.command;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
+import nexusvault.cli.core.App;
+import nexusvault.cli.core.AutoInstantiate;
+import nexusvault.cli.core.cmd.AbstractCommandHandler;
+import nexusvault.cli.core.cmd.Argument;
+import nexusvault.cli.core.cmd.Arguments;
+import nexusvault.cli.core.cmd.CommandDescription;
+import nexusvault.cli.extensions.convert.ConversionRequest;
+import nexusvault.cli.extensions.convert.ConverterExtension;
+import nexusvault.cli.extensions.convert.ConverterOptions;
+import nexusvault.cli.extensions.convert.resource.FileResource;
+import nexusvault.cli.extensions.convert.resource.Resource;
+
+@AutoInstantiate
+public final class ConvertFile extends AbstractCommandHandler {
+
+	@Override
+	public CommandDescription getCommandDescription() {
+		// @formatter:off
+		return CommandDescription.newInfo()
+				.setCommandName("convert-file")
+				.setDescription("Converts files from one format to another.")
+				.setNoNamedArguments()
+				.build();
+//				.addNamedArgument(ArgumentDescription.newInfo()
+//						.setName("file")
+//						.setDescription("One or more files for conversion, this depends on the converter")
+//						.setRequired(true)
+//						.setArguments(false)
+//						.setNumberOfArgumentsUnlimited()
+//						.setNamesOfArguments("files,...")
+//						.build())
+//				.namedArgumentsDone()
+//				.build();
+		//@formatter:on
+	}
+
+	@Override
+	public void onCommand(Arguments args) {
+		if (args.getUnnamedArgumentSize() == 0) {
+			sendMsg(() -> String.format("At least one file path is required"));
+			return;
+		}
+
+		final var targets = new LinkedList<Resource>();
+		boolean allFilesFound = true;
+
+		for (final var strPath : args.getUnnamedArgs()) {
+			final var path = Paths.get(strPath);
+			if (Files.exists(path)) {
+				targets.add(new FileResource(path));
+			} else {
+				sendMsg(() -> String.format("File not found: %s", path));
+				allFilesFound = false;
+			}
+		}
+
+		if (!allFilesFound) {
+			sendMsg(() -> "Missing files. Abort convertion.");
+			return;
+		}
+
+		final var options = new ConverterOptions(Arrays.stream(args.getNamedArgs()).collect(Collectors.toMap(Argument::getName, Argument::getValues)));
+		final var requests = targets.stream().map(e -> new ConversionRequest(e, null)).collect(Collectors.toList());
+		final var results = App.getInstance().getExtension(ConverterExtension.class).convert(requests, options, null);
+
+		if (results.stream().anyMatch(e -> e.isFailed())) {
+			sendMsg(() -> {
+				final StringBuilder msg = new StringBuilder();
+				msg.append("Unable to convert file(s)'\n");
+				for (final var result : results) {
+					if (result.isFailed()) {
+						msg.append(result.getRequest().input.getFilePath()).append("\n");
+						msg.append("->").append(result.getError().getClass());
+						msg.append(" : ").append(result.getError().getMessage());
+						msg.append("\n");
+
+						var cause = result.getError().getCause();
+						while (cause != null) {
+							msg.append("\t\t->").append(cause.getClass()).append(" : ").append(cause.getMessage()).append("\n");
+							cause = cause.getCause();
+						}
+					}
+				}
+				return msg.toString();
+			});
+		}
+	}
+
+	@Override
+	public String onHelp() {
+		final var extension = App.getInstance().getExtension(ConverterExtension.class);
+		final var builder = new StringBuilder();
+		builder.append("Supported file conversions:").append("\n");
+		for (final var fileExtension : extension.getSupportedFileExtensions()) {
+			final var converters = new HashSet<>(extension.getConvertersForFileExtensions(fileExtension));
+			final var preferredConverter = extension.getPreferredConverterForFileExtension(fileExtension);
+			converters.remove(preferredConverter);
+			if (preferredConverter != null) {
+				converters.add("[" + preferredConverter + "]");
+			}
+			builder.append("\t").append(fileExtension).append(" - > ").append(String.join(", ", converters));
+		}
+		return builder.toString();
+	}
+
+}
